6d0b61389426e09cc48288d61c5799f031eead62
snownets <snownesst@gmail.com>
2017-11-04 20:07:59 -0600

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEJ05poyDk60qRwADE6itR4vogHC0Kegk",
+  authDomain: "medel-512f2.firebaseapp.com",
+  databaseURL: "https://medel-512f2.firebaseio.com",
+  projectId: "medel-512f2",
+  storageBucket: "medel-512f2.appspot.com",
+  messagingSenderId: "116990531599"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +41,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
